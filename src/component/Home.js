a5050a3d0d68a3d1569bcba0f1c2eb1f224a26cb
Mintesn <mketema4@gmail.com>
2024-04-15 01:09:19 +0300

    first page

---
+++ src/component/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './home.css'
+
+
+function Home() {
+  return (
+    <div>
+      <nav className='navbar'>
+        <h3 className='logo'>AMAZING NEW BRAND</h3>
+        <ul>
+            <li>HOME</li>
+            <li>CLENTS</li>
+            <li>BLOG</li>
+            <li>CONTACT</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Home
